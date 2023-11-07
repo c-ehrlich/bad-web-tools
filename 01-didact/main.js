12b04da1fb94a7ab567fb340842b03e4edcf1793
c-ehrlich <ehrlich.christopher@gmail.com>
2023-11-07 19:17:20 +0100

    didact: render a tree

--- 01-didact/main.js
+++ 01-didact/main.js
@@ -1,25 +1,81 @@
-console.log("hi");
-
-const element = {
-  type: "h1",
-  props: {
-    title: "foo",
-    children: "Hello",
-  },
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child =>
+        typeof child === "object"
+          ? child
+          : createTextElement(child)
+      )
+    }
+  }
 }
 
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: []
+    }
+  }
+}
+
+function render(element, container) {
+  const dom = element.type === "TEXT_ELEMENT"
+    ? document.createTextNode(element.props.nodeValue)
+    : document.createElement(element.type);
+  
+  const { children, ...otherProps } = element.props;
+  Object.assign(dom, otherProps);
+
+  // TODO: make concurrent
+  children.forEach(child => render(child, dom));
+
+  container.appendChild(dom);
+}
+
+// TODO: does react also use a global for this?
+let nextUnitOfWork = null;
+
+function workLoop(deadline) {
+  let shouldYield = false;
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+  requestIdleCallback(workLoop);
+}
+
+requestIdleCallback(workLoop);
+
+function performUnitOfWork(nextUnitOfWork) {
+  // TODO: do work
+}
+
+const Didact = {
+  createElement,
+  render,
+}
+
+const element = Didact.createElement(
+  "div",
+  { id: "foo" },
+  Didact.createElement("a", null, "bar"),
+  Didact.createElement("b")
+)
+
 const container = document.getElementById("root");
+Didact.render(element, container);
 
-// element: React element
-// node: DOM element
+// const node = document.createElement(element.type);
+// node["title"] = element.props.title;
 
-const node = document.createElement(element.type);
-node["title"] = element.props.title;
+// const text = document.createTextNode("");
+// text["nodeValue"] = element.props.children;
 
-// https://developer.mozilla.org/en-US/docs/Web/API/Node
-// using textNode instead of setting innerText lets us treat all elements in the same way later
-const text = document.createTextNode("");
-text["nodeValue"] = element.props.children
+// node.appendChild(text);
+// container.appendChild(node);
 
-node.appendChild(text);
-container.appendChild(node);
+console.log("hi");
